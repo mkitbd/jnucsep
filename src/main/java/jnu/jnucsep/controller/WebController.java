4b9365a4926825068252015fa415343ceff74363
Md. Mostafa Kamal <86487207+mkitbd@users.noreply.github.com>
2022-04-28 12:02:30 +0600

    student save

--- src/main/java/jnu/jnucsep/controller/WebController.java
+++ src/main/java/jnu/jnucsep/controller/WebController.java
@@ -1,12 +1,17 @@
 package jnu.jnucsep.controller;
 
 import jnu.jnucsep.model.Student;
+import jnu.jnucsep.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class WebController {
+    @Autowired
+    private StudentService studentService;
+
     @RequestMapping(value = "/index")
     public String index() {
         return "index";
@@ -21,6 +26,12 @@ public class WebController {
     public String showStudent (Model model){
         Student student = new Student();
         model.addAttribute("student",student);
+        return "student";
+    }
+
+    @PostMapping("/saveStudent")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
         return "student";
     }
 
